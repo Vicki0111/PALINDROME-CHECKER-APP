4a1185f87532c5f3889ce68d18ee054dd57200f4
Vicki0111 <vicvignesh0111@gmail.com>
2026-02-24 14:43:57 +0530

    UC1: Add welcome message module

---
+++ src/uc1.java
@@ -0,0 +1,7 @@
+public class uc1 {
+    public static void main(String[] args){
+        System.out.println("Welcome to the Paldindrome Checker Management System");
+        System.out.println("Version 1.0");
+        System.out.println("System initialized successfully.");
+    }
+}
---
+++ src/uc2.java
@@ -0,0 +1,19 @@
+public class uc2 {
+    public static void main(String[] args) {
+
+        String text = "hello";
+        text = text.toLowerCase();
+
+        String reversed = "";
+
+        for (int i = text.length() - 1; i >= 0; i--) {
+            reversed += text.charAt(i);
+        }
+
+        if (text.equals(reversed)) {
+            System.out.println(text + " is a palindrome!");
+        } else {
+            System.out.println(text + " is NOT a palindrome.");
+        }
+    }
+}
